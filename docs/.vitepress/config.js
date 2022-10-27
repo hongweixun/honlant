2ecbf82c48a6513aec5bacdade72c02c495a4b6b
宏联搬砖工 <java_hongwx@163.com>
2022-10-27 11:15:28 +0800

    base

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,12 +4,12 @@
  * @Author: hongweixun
  * @Date: 2022-10-26 10:54:47
  * @LastEditors: hongweixun
- * @LastEditTime: 2022-10-27 10:24:20
+ * @LastEditTime: 2022-10-27 11:13:29
  */
 module.exports = {
   title: 'HonLant',
   description: 'Just playing around.',
-  base:'/honlant/',
+  base:'./',
   themeConfig: {
     logo: '/public/favicon.ico',
     // 展示搜索框
