39655f86cc3d19b907e409756063e66c110ad60e
宏联搬砖工 <java_hongwx@163.com>
2022-10-27 11:19:42 +0800

    base

---
+++ assets/guide_news_index.md.d24f8a47.js
@@ -0,0 +1,8 @@
+import{_ as n,c as a,a as o,d as e,e as t,o as d}from"./app.a7ffa660.js";const x='{"title":"\u5173\u4E8E\u7248\u672C","description":"","frontmatter":{},"relativePath":"guide/news/index.md","lastUpdated":1666840636512}',r={},s=e("h1",{id:"\u5173\u4E8E\u7248\u672C",tabindex:"-1"},[t("\u5173\u4E8E\u7248\u672C "),e("a",{class:"header-anchor",href:"#\u5173\u4E8E\u7248\u672C","aria-hidden":"true"},"#")],-1),c=e("p",null,[t("\u76EE\u524D\u5F00\u53D1\u52300.0.5\u7248\u672C\uFF0C\u4E3B\u8981\u6A21\u5757\u4E3A"),e("code",null,"button"),t(","),e("code",null,"loading"),t(","),e("code",null,"card"),t(","),e("code",null,"model"),t(",\u66F4\u591A\u529F\u80FD\u5728\u8FED\u4EE3\u4E2D")],-1);function i(l,u,p,_,h,f){return d(),a("div",null,[o(`
+ * @Descripttion: your project
+ * @version: 1.0
+ * @Author: hongweixun
+ * @Date: 2022-10-25 13:41:36
+ * @LastEditors: hongweixun
+ * @LastEditTime: 2022-10-25 13:43:18
+`),s,c])}var g=n(r,[["render",i]]);export{x as __pageData,g as default};
