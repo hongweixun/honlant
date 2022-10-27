d2f6c7c026d05b421458fdb251bff2ad7f5ba64a
宏联搬砖工 <java_hongwx@163.com>
2022-10-27 11:55:56 +0800

    2

---
+++ assets/components_installation_index.md.3f37271e.lean.js
@@ -0,0 +1,8 @@
+import{_ as a,c as e,a as n,f as t,o as r}from"./app.a29bcca4.js";const m='{"title":"\u5B89\u88C5","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u73AF\u5883\u652F\u6301","slug":"\u73AF\u5883\u652F\u6301"},{"level":2,"title":"\u7248\u672C","slug":"\u7248\u672C"},{"level":2,"title":"\u4F7F\u7528\u5305\u7BA1\u7406\u5668","slug":"\u4F7F\u7528\u5305\u7BA1\u7406\u5668"},{"level":2,"title":"\u6D4F\u89C8\u5668\u76F4\u63A5\u5F15\u5165","slug":"\u6D4F\u89C8\u5668\u76F4\u63A5\u5F15\u5165"}],"relativePath":"components/installation/index.md","lastUpdated":1666841839047}',i={},d=t('',18);function h(s,o,c,p,l,u){return r(),e("div",null,[n(`
+ * @Descripttion: your project
+ * @version: 1.0
+ * @Author: hongweixun
+ * @Date: 2022-10-24 14:44:50
+ * @LastEditors: hongweixun
+ * @LastEditTime: 2022-10-25 09:24:14
+`),d])}var f=a(i,[["render",h]]);export{m as __pageData,f as default};
